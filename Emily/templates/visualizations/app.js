a5d95da9834eed074adf6af8440876c628a32627
unknown <tony.gardella5@gmail.com>
2020-10-12 21:25:59 -0500

    created page for line graph and placed a filler graph in for now

--- Emily/templates/visualizations/app.js
+++ Emily/templates/visualizations/app.js
@@ -19,10 +19,10 @@
           title: {
             text: 'Traffic Sources'
           },
-          dataLabels: {
-            enabled: true,
-            enabledOnSeries: [1]
-          },
+          //dataLabels: {
+          //  enabled: true,
+          //  enabledOnSeries: [1]
+          //},
           labels: ['01 Jan 2001', '02 Jan 2001', '03 Jan 2001', '04 Jan 2001', '05 Jan 2001', '06 Jan 2001', '07 Jan 2001', '08 Jan 2001', '09 Jan 2001', '10 Jan 2001', '11 Jan 2001', '12 Jan 2001'],
           xaxis: {
             type: 'datetime'

cb6c0542f089b8b5e0739f5d76cf15d064adab3a
Tony Gardella <Tony@Tonys-MacBook-Pro.local>
2020-10-18 18:59:39 -0500

    working if you choose a new state each time. working on making it work for county.

--- Emily/templates/static/js/app.js
+++ Emily/templates/static/js/app.js
@@ -3,8 +3,8 @@ d3.csv("/../../../../data/final_data.csv", d3.autoType).then(function(allData) {
   var deathAxis = [];
   var casesAxis = [];
   var xAxis = [];
-  var chosenCounty = "St. Louis";
-  var chosenState = "MO"
+  var chosenCounty = "Sussex";
+  var chosenState = "DE"
   var allState = [];
   var allCounty = [];
   var uniqueState = [];
@@ -19,7 +19,7 @@ d3.csv("/../../../../data/final_data.csv", d3.autoType).then(function(allData) {
       casesAxis.push(allData[i].cases);
       xAxis.push(allData[i].date);
   }}};
-function newCountyList(){
+/*function newCountyList(){
   var allCounty = [];
   for (var i=0; i < allData.length; i++) {
      
@@ -32,15 +32,15 @@ function newCountyList(){
   }}}
   var uniqueCounty = [...new Set(allCounty)];
     uniqueCounty.sort();
-    console.log(uniqueCounty);
+    //console.log(uniqueCounty);
 
-}
+}*/
   var uniqueState = [...new Set(allState)];
     uniqueState.sort();
-    console.log(uniqueState);
+    //console.log(uniqueState);
   var uniqueCounty = [...new Set(allCounty)];
     uniqueCounty.sort();
-    console.log(uniqueCounty);
+    //console.log(uniqueCounty);
 
   /*var select = document.getElementById("state"); 
      
@@ -66,9 +66,10 @@ for(var i = 0; i < options.length; i++) {
 }
 }
 function updateState(){
+  chart.destroy();
   var e = document.getElementById("state");
     chosenState = e.options[e.selectedIndex].text;
-    console.log(chosenState);
+    //console.log(chosenState);
     
   var allCounty = [];
   for (var i=0; i < allData.length; i++) {
@@ -82,7 +83,7 @@ function updateState(){
   }}}
   var uniqueCounty = [...new Set(allCounty)];
     uniqueCounty.sort();
-    console.log(uniqueCounty);
+    //console.log(uniqueCounty);
 
     removeOptions(document.getElementById("county"));
     var select = document.getElementById("county");
@@ -117,62 +118,154 @@ function createCounty(uniqueCounty) {
   }
   }
 function updateCounty(){
+  //chart.destroy();
+  deathAxis=[];
+  casesAxis=[];
+  xAxis=[];
+  //chart.destroy();
 var e = document.getElementById("county");
   chosenCounty = e.options[e.selectedIndex].text;
-  console.log(chosenCounty);
+  //console.log(chosenCounty);
+  var allCounty = [];
+  for (var i=0; i < allData.length; i++) {
+     
+    if( allData[i].state == chosenState)
+    { allCounty.push(allData[i].county);
+       if (allData[i].county == chosenCounty){
+      deathAxis.push(allData[i].deaths);
+      casesAxis.push(allData[i].cases);
+      xAxis.push(allData[i].date);
+  }}};
+  //console.log(casesAxis);
+//reset chart
+//var chart = new ApexCharts(document.querySelector("#chart"), options);
+//chart.destroy();
+var options = {
+  series: [{
+  name: 'Deaths',
+  type: 'column',
+  data: deathAxis
+}, {
+  name: 'Cases',
+  type: 'line',
+  data: casesAxis
+}],
+  chart: {
+  height: 400,
+  type: 'line',
+},
+stroke: {
+  width: [0, 5]
+},
+title: {
+  text: `COVID-19 Cases and Deaths for ${chosenCounty}, ${chosenState}`
+},
+
+labels: xAxis,
+xaxis: {
+  type: 'datetime'
+},
+yaxis: [{
+  title: {
+    text: 'Cases',
+  },
+
+}, {
+  opposite: true,
+  title: {
+    text: 'Deaths'
+  }
+}]
+};
+//chart.updateSeries([]);
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+chart.render(); 
+/*chart.updateOptions({
+  title: {
+    text: `COVID-19 Cases and Deaths for ${chosenCounty}, ${chosenState}`
+  }
+});
+chart.updateSeries([{
+  name:"Cases",
+  data: casesAxis
   
-}
+}]);
+
+chart.updateSeries([{
+  name:"Deaths",
+  data: deathAxis
+  
+}]);
+*/
+/*chart.updateSeries([{
+  series: [{
+    name: 'Deaths',
+    type: 'column',
+    data: deathAxis
+  }, {
+    name: 'Cases',
+    type: 'line',
+    data: casesAxis
+  }]
+  
+}]);*/
+
+};
+
+
 
 createCounty(uniqueCounty);
-updateCounty();
+//updateCounty();
 createState();
-updateState();
+//updateState();
 //create functions for updating. you are going to need to append the dropdown lists with unique
 //lists.
+//render chart
+var options = {
+  series: [{
+  name: 'Deaths',
+  type: 'column',
+  data: deathAxis
+}, {
+  name: 'Cases',
+  type: 'line',
+  data: casesAxis
+}],
+  chart: {
+  height: 400,
+  type: 'line',
+},
+stroke: {
+  width: [0, 5]
+},
+title: {
+  text: `COVID-19 Cases and Deaths for ${chosenCounty}, ${chosenState}`
+},
+
+labels: xAxis,
+xaxis: {
+  type: 'datetime'
+},
+yaxis: [{
+  title: {
+    text: 'Cases',
+  },
+
+}, {
+  opposite: true,
+  title: {
+    text: 'Deaths'
+  }
+}]
+};
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render(); 
 
 
-
-    var options = {
-      series: [{
-      name: 'Deaths',
-      type: 'column',
-      data: deathAxis
-    }, {
-      name: 'Cases',
-      type: 'line',
-      data: casesAxis
-    }],
-      chart: {
-      height: 400,
-      type: 'line',
-    },
-    stroke: {
-      width: [0, 5]
-    },
-    title: {
-      text: `COVID-19 Cases and Deaths for ${chosenCounty}, ${chosenState}`
-    },
-
-    labels: xAxis,
-    xaxis: {
-      type: 'datetime'
-    },
-    yaxis: [{
-      title: {
-        text: 'Cases',
-      },
-    
-    }, {
-      opposite: true,
-      title: {
-        text: 'Deaths'
-      }
-    }]
-    };
-    //console.log(deathAxis);
-    var chart = new ApexCharts(document.querySelector("#chart"), options);
-    chart.render();
+   
     d3.select("#state").on("change", updateState);
+   // d3.select("#county").on("change", chart.destroy());
     d3.select("#county").on("change", updateCounty);
     
 

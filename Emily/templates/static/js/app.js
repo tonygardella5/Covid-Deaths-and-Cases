fb034da5c4031aa8855b20fbab96e2b5d90c5253
Tony Gardella <Tony@Tonys-MacBook-Pro.local>
2020-10-18 14:52:48 -0500

    dropdown menus functioning and updating properly

--- Emily/templates/static/js/app.js
+++ Emily/templates/static/js/app.js
@@ -3,10 +3,12 @@ d3.csv("/../../../../data/final_data.csv", d3.autoType).then(function(allData) {
   var deathAxis = [];
   var casesAxis = [];
   var xAxis = [];
-  var chosenCounty = "Davidson";
-  var chosenState = "TN"
+  var chosenCounty = "St. Louis";
+  var chosenState = "MO"
   var allState = [];
   var allCounty = [];
+  var uniqueState = [];
+  var uniqueCounty = [];
   
   for (var i=0; i < allData.length; i++) {
     allState.push(allData[i].state);    
@@ -17,22 +19,119 @@ d3.csv("/../../../../data/final_data.csv", d3.autoType).then(function(allData) {
       casesAxis.push(allData[i].cases);
       xAxis.push(allData[i].date);
   }}};
-
-const uniqueState = [...new Set(allState)];
-    console.log(uniqueState);
-const uniqueCounty = [...new Set(allCounty)];
+function newCountyList(){
+  var allCounty = [];
+  for (var i=0; i < allData.length; i++) {
+     
+    if( allData[i].state == chosenState)
+    { allCounty.push(allData[i].county);
+       if (allData[i].county == chosenCounty){
+      deathAxis.push(allData[i].deaths);
+      casesAxis.push(allData[i].cases);
+      xAxis.push(allData[i].date);
+  }}}
+  var uniqueCounty = [...new Set(allCounty)];
+    uniqueCounty.sort();
     console.log(uniqueCounty);
 
-function updateState() {
-
 }
-function updateCounty() {
+  var uniqueState = [...new Set(allState)];
+    uniqueState.sort();
+    console.log(uniqueState);
+  var uniqueCounty = [...new Set(allCounty)];
+    uniqueCounty.sort();
+    console.log(uniqueCounty);
 
+  /*var select = document.getElementById("state"); 
+     
+  for(var i = 0; i < uniqueState.length; i++) {
+      var opt = uniqueState[i];
+      var el = document.createElement("option");
+      el.textContent = opt;
+      el.value = opt;
+      select.appendChild(el);
+  }​
+*/
+
+
+function createState() {
+  var select = document.getElementById("state");
+var options = uniqueState;
+for(var i = 0; i < options.length; i++) {
+    var opt = options[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.append(el);
 }
+}
+function updateState(){
+  var e = document.getElementById("state");
+    chosenState = e.options[e.selectedIndex].text;
+    console.log(chosenState);
+    
+  var allCounty = [];
+  for (var i=0; i < allData.length; i++) {
+     
+    if( allData[i].state == chosenState)
+    { allCounty.push(allData[i].county);
+       if (allData[i].county == chosenCounty){
+      deathAxis.push(allData[i].deaths);
+      casesAxis.push(allData[i].cases);
+      xAxis.push(allData[i].date);
+  }}}
+  var uniqueCounty = [...new Set(allCounty)];
+    uniqueCounty.sort();
+    console.log(uniqueCounty);
+
+    removeOptions(document.getElementById("county"));
+    var select = document.getElementById("county");
+  var options = uniqueCounty;
+  for(var i = 0; i < options.length; i++) {
+      var opt = options[i];
+      var el = document.createElement("option");
+      el.textContent = opt;
+      el.value = opt;
+      select.append(el);
+  }
+    
+    
+    
+  }
+function removeOptions(selectElement) {
+    var i, L = selectElement.options.length - 1;
+    for(i = L; i >= 0; i--) {
+       selectElement.remove(i);
+    }
+ }
+function createCounty(uniqueCounty) {
+
+ var select = document.getElementById("county");
+  var options = uniqueCounty;
+  for(var i = 0; i < options.length; i++) {
+      var opt = options[i];
+      var el = document.createElement("option");
+      el.textContent = opt;
+      el.value = opt;
+      select.append(el);
+  }
+  }
+function updateCounty(){
+var e = document.getElementById("county");
+  chosenCounty = e.options[e.selectedIndex].text;
+  console.log(chosenCounty);
+  
+}
+
+createCounty(uniqueCounty);
+updateCounty();
+createState();
+updateState();
 //create functions for updating. you are going to need to append the dropdown lists with unique
 //lists.
-d3.select("state").on("change", updateState);
-d3.select("county").on("change", updateCounty);
+
+
+
     var options = {
       series: [{
       name: 'Deaths',
@@ -73,11 +172,13 @@ d3.select("county").on("change", updateCounty);
     //console.log(deathAxis);
     var chart = new ApexCharts(document.querySelector("#chart"), options);
     chart.render();
+    d3.select("#state").on("change", updateState);
+    d3.select("#county").on("change", updateCounty);
+    
 
 
 
-
-
+   
 
 
 });
